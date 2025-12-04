09a26d60148c0a52f0261ec11e9358293c9b03cb
SalmaIdja <salmaidja2018@gmail.com>
2025-12-04 16:11:22 -0600

    Create revenue_by_country.sql

---
+++ queries/queries/revenue_by_country.sql
@@ -0,0 +1,7 @@
+-- Revenue by country
+SELECT c.country, SUM(s.quantity * p.price) AS revenue
+FROM Sales s
+JOIN Customers c ON s.customer_id = c.customer_id
+JOIN Products p ON s.product_id = p.product_id
+GROUP BY c.country
+ORDER BY revenue DESC;
