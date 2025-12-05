62ebed44621b040bc1a747d2b3e897d5fdc8fffa
SalmaIdja <salmaidja2018@gmail.com>
2025-12-05 11:37:14 -0600

    Delete queries directory

--- queries/queries/queries/monthly_sales.sql
+++
@@ -1,7 +0,0 @@
--- Monthly sales totals (MySQL syntax)
-SELECT DATE_FORMAT(sale_date, '%Y-%m') AS month,
-       SUM(s.quantity * p.price) AS monthly_revenue
-FROM Sales s
-JOIN Products p ON s.product_id = p.product_id
-GROUP BY month
-ORDER BY month;
--- queries/top_products.sql
+++
@@ -1,6 +0,0 @@
--- Top-selling products
-SELECT p.product_name, SUM(s.quantity) AS total_sold
-FROM Sales s
-JOIN Products p ON s.product_id = p.product_id
-GROUP BY p.product_name
-ORDER BY total_sold DESC;
