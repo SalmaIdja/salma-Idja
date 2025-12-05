a61bae8cff8f9cca18338558cb69c29ff6d15b0b
SalmaIdja <salmaidja2018@gmail.com>
2025-12-05 11:50:38 -0600

    Create most_borrowed_books.sql

---
+++ queries/most_borrowed_books.sql
@@ -0,0 +1,9 @@
+-- Count how many times each book was borrowed
+SELECT
+    b.title,
+    b.author,
+    COUNT(l.loan_id) AS times_borrowed
+FROM Loans l
+JOIN Books b ON l.book_id = b.book_id
+GROUP BY b.book_id, b.title, b.author
+ORDER BY times_borrowed DESC;
